ea8f3090836afff5b4a1f81196a6d5c1a148f178
lukasrplus <nordspitz@desktop-4fgsita.buero.local>
2016-03-18 13:47:56 +0100

    Vorbereitung für User

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/welcome'
+
   resources :bulbs
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#welcome'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
